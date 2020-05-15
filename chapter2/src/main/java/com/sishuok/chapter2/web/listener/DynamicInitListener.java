0e4206845eaa6eabe1c8935bd9021a67f2968065
xiaojihua <2542213767@qq.com>
2020-05-15 11:08:38 +0800

    20200515

--- chapter2/src/main/java/com/sishuok/chapter2/web/listener/DynamicInitListener.java
+++ chapter2/src/main/java/com/sishuok/chapter2/web/listener/DynamicInitListener.java
@@ -15,27 +15,32 @@ import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.WebListener;
 
 /**
- * <p>User: Zhang Kaitao
- * <p>Date: 13-6-21 下午4:02
- * <p>Version: 1.0
+ * 动态注册listener,filter以及servlet
  */
 @WebListener
 public class DynamicInitListener implements ServletContextListener {
+
+    /**
+     * 需要在容器的时候，通过监听器来初始化。
+     * 通过ServletContext来注册
+     * @param sce
+     */
     @Override
     public void contextInitialized(final ServletContextEvent sce) {
+        // 获得servletContext
         ServletContext sc = sce.getServletContext();
-
+        // 注册listener
         //sc.addListener("com.sishuok.chapter2.web.listener.DynamicServletContextListener");
-
+        // 注册filter
         sc.addFilter("dynamicFilter", DynamicFilter.class);
-
+        // 注册Servlet方式1
         ServletRegistration.Dynamic dynamic1 = sc.addServlet("dynamicServlet1", DynamicServlet.class);
         dynamic1.setLoadOnStartup(1);
         dynamic1.addMapping("/dynamic1");
-
+        // 注册Servlet方式2
         ServletRegistration.Dynamic dynamic2 = sc.addServlet("dynamicServlet2", "com.sishuok.chapter2.web.servlet.DynamicServlet");
         dynamic2.addMapping("/dynamic2");
-
+        // 注册Servlet方式3
         ServletRegistration.Dynamic dynamic3 = sc.addServlet("dynamicServlet3", new DynamicServlet());
         dynamic3.addMapping("/dynamic3");
 
